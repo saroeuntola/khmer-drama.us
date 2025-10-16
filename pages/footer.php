016b1317b6c7087d28520d92ba03177de0e4c225
tola <tolasweatboy@gmail.com>
2025-10-16 15:18:06 +0700

    update

--- pages/footer.php
+++ pages/footer.php
@@ -10,30 +10,34 @@
         <div class="flex space-x-6 text-gray-300 text-lg mb-4 md:mb-0">
             <a href="/pages/about-us" class="hover:text-indigo-400 transition-colors duration-200">About Us</a>
             <a href="/pages/privacy-policy" class="hover:text-indigo-400 transition-colors duration-200">Privacy Policy</a>
-           
+
         </div>
 
         <!-- Social Media Icons -->
-        <div class="flex space-x-4">
-            <!-- Facebook -->
-            <a href="https://facebook.com/yourpage" target="_blank" class="hover:text-indigo-400 transition-colors duration-200">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M22 12.07c0-5.5-4.47-9.97-9.97-9.97S2.06 6.57 2.06 12.07c0 4.99 3.66 9.13 8.44 9.88v-6.99h-2.54v-2.89h2.54V9.89c0-2.5 1.49-3.88 3.77-3.88 1.09 0 2.23.2 2.23.2v2.45h-1.25c-1.23 0-1.61.76-1.61 1.54v1.84h2.74l-.44 2.89h-2.3v6.99c4.78-.75 8.44-4.89 8.44-9.88z" />
-                </svg>
-            </a>
-            <!-- Twitter -->
-            <a href="https://twitter.com/yourprofile" target="_blank" class="hover:text-indigo-400 transition-colors duration-200">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 4.56c-.89.39-1.85.65-2.85.77a4.97 4.97 0 0 0 2.17-2.74c-.95.56-2 .97-3.12 1.19A4.93 4.93 0 0 0 16.67 3c-2.73 0-4.95 2.22-4.95 4.95 0 .39.04.77.13 1.13C7.69 8.98 4.07 7.13 1.64 4.15a4.93 4.93 0 0 0-.67 2.49c0 1.71.87 3.22 2.18 4.11a4.9 4.9 0 0 1-2.24-.62v.06c0 2.4 1.71 4.4 3.97 4.85a4.94 4.94 0 0 1-2.22.08c.63 1.96 2.45 3.38 4.6 3.42A9.88 9.88 0 0 1 0 19.54a13.94 13.94 0 0 0 7.55 2.21c9.06 0 14-7.5 14-14v-.64A10.01 10.01 0 0 0 24 4.56z" />
-                </svg>
-            </a>
-            <!-- YouTube -->
-            <a href="https://youtube.com/yourchannel" target="_blank" class="hover:text-indigo-400 transition-colors duration-200">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M23.5 6.2c-.3-1-1.2-1.8-2.2-2C19.4 3.9 12 3.9 12 3.9s-7.4 0-9.3.3c-1 .2-1.9 1-2.2 2C.1 8.1 0 12 0 12s.1 3.9.3 5.8c.3 1 1.2 1.8 2.2 2C4.6 20.1 12 20.1 12 20.1s7.4 0 9.3-.3c1-.2 1.9-1 2.2-2 .2-1.9.3-5.8.3-5.8s-.1-3.9-.3-5.8zM9.8 15.5V8.5l6.2 3.5-6.2 3.5z" />
-                </svg>
-            </a>
+        <!-- Contact Section -->
+        <div class="flex flex-col items-start space-y-2">
+
+
+            <!-- Social Media Icons -->
+            <div class="flex space-x-4 items-center">
+
+                <!-- Facebook -->
+                <a href="https://facebook.com/yourpage" target="_blank" class="hover:opacity-70 transition-colors duration-200" title="Facebook">
+                    <img src="../assets/img/fb.png" alt="facebook logo" class="w-6">
+                </a>
+
+                <!-- Telegram -->
+                <a href="https://t.me/yourusername" target="_blank" class="hover:opacity-70 transition-colors duration-200" title="Telegram">
+                    <img src="../assets/img/tele.png" alt="telegram logo" class="w-6">
+                </a>
+
+                <!-- LinkedIn -->
+                <a href="https://linkedin.com/in/yourprofile" target="_blank" class="hover:opacity-70 transition-colors duration-200" title="LinkedIn">
+                    <img src="../assets/img/linkedin.svg" alt="linkedin logo" class="w-6">
+                </a>
+            </div>
         </div>
+
     </div>
 
     <!-- Copyright -->
