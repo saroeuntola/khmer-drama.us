e3e8655b63fed599345c1bf9b2a502effafdf682
tola <tolasweatboy@gmail.com>
2025-11-19 11:09:12 +0700

    update

--- index.php
+++ index.php
@@ -106,6 +106,8 @@
         }
     </script>
 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9457393184068455"
+        crossorigin="anonymous"></script>
 </head>
 
 <body class="bg-gray-900 text-white">
